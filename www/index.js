a1eaa2e9039735fa4ea795e7aef8119806ce0d3c
unknown <think.actions@gmail.com>
2016-03-18 01:53:33 +0700

    masih coba coba 6

--- www/index.js
+++ www/index.js
@@ -1,30 +1,12 @@
 document.addEventListener('deviceready',onDeviceReady, false);
-function checkConnection() {
-           var networkState = navigator.network.connection.type;
-           var states = {};
-           states[Connection.UNKNOWN]  = 'Unknown connection';
-           states[Connection.ETHERNET] = 'Ethernet connection';
-           states[Connection.WIFI]     = 'WiFi connection';
-           states[Connection.CELL_2G]  = 'Cell 2G connection';
-           states[Connection.CELL_3G]  = 'Cell 3G connection';
-           states[Connection.CELL_4G]  = 'Cell 4G connection';
-           states[Connection.NONE]     = 'No network connection';
-
-           return networkState;
-
-       }
 function onDeviceReady(){
-var networkState = checkConnection();
-/*load local files if there is not network connection */
-                  if (networkState == Connection.NONE) {
-                   navigator.notification.alert('This app requires an internet connection');
-                  } else {
-                 window.location="http://app.klikdieng.com";
-                  }
-document.addEventListener("menubutton",function(){console.log("The menu was clicked...");});
-
-};
+hidefrontn();
+//var ref = window.open('http://app.klikdieng.com', '_self','location=no,zoom=no,toolbar=no,hidden=no,hardwareback=no');
+document.addEventListener('menubutton', onMenuKeyDown, false);
+}
 function showspinner(){window.plugins.spinnerDialog.show(null, "loading bentar..");}
 function hidespinner(){window.plugins.spinnerDialog.hide(); }
+function onMenuKeyDown(){$('#menubot').show();}
+function hidefrontn(){$('.listening').hide();}
 
 
