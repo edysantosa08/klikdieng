b8c9473391b466c8e27ebffbfd7af4e835006c31
unknown <think.actions@gmail.com>
2016-03-18 02:03:46 +0700

    masih coba coba 7

--- www/index.js
+++ www/index.js
@@ -1,7 +1,15 @@
 document.addEventListener('deviceready',onDeviceReady, false);
 function onDeviceReady(){
 hidefrontn();
-//var ref = window.open('http://app.klikdieng.com', '_self','location=no,zoom=no,toolbar=no,hidden=no,hardwareback=no');
+ref = window.open('http://app.klikdieng.com', '_self','location=no,zoom=no,toolbar=no,hidden=no,hardwareback=no');
+ref.addEventListener("loadstart", function() {
+showspinner();
+});
+ref.addEventListener("loadstop", function() {
+ref.show();
+hidespinner();
+});
+navigator.app.overrideButton("menubutton", true);
 document.addEventListener('menubutton', onMenuKeyDown, false);
 }
 function showspinner(){window.plugins.spinnerDialog.show(null, "loading bentar..");}
