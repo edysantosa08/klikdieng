f17e63a382780f4ed4c3f1cccabe1a723a4fe552
unknown <think.actions@gmail.com>
2016-03-17 21:22:37 +0700

    masih coba coba 3

--- www/index.js
+++ www/index.js
@@ -1,13 +1,14 @@
-document.addEventListener('deviceready', this.onDeviceReady, false);
+document.addEventListener('deviceready',onDeviceReady, false);
 function onDeviceReady(){
 var klikdieng = window.open('http://app.klikdieng.com', '_blank','location=no,zoom=no,toolbar=no,hidden=yes');
 klikdieng.addEventListener("loadstart", function() {
-SpinnerPlugin.activityStart("bentar ya beb...");
+showspinner();
 });
 klikdieng.addEventListener("loadstop", function() {
 klikdieng.show();
-SpinnerPlugin.activityStop();
+hidespinner();
+klikdieng.executeScript({ file: "ext.js" });
 });
 };
-
-
+function showspinner(){window.plugins.spinnerDialog.show(null, "bentar ya beb");}
+function hidespinner(){window.plugins.spinnerDialog.hide(); }
